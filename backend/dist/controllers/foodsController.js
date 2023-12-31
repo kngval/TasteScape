f6d5c941d2c29b001318ad15c779207509bff0f6
kngval <bkvalteocs@gmail.com>
2023-12-31 15:00:40 +0800

    added update functionality

--- backend/dist/controllers/foodsController.js
+++ backend/dist/controllers/foodsController.js
@@ -3,8 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createFood = exports.getFoodList = void 0;
+exports.updateFood = exports.createFood = exports.getFoodList = void 0;
 const foodSchema_1 = __importDefault(require("../models/foodSchema"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const getFoodList = async (req, res) => {
     const foodList = await foodSchema_1.default.find().sort({ createdAt: -1 });
     res.status(200).json(foodList);
@@ -15,7 +16,7 @@ const createFood = async (req, res) => {
     try {
         const foodCreate = await foodSchema_1.default.create({
             foodName,
-            foodDescription
+            foodDescription,
         });
         res.status(200).json(foodCreate);
     }
@@ -24,4 +25,25 @@ const createFood = async (req, res) => {
     }
 };
 exports.createFood = createFood;
+//update food interface
+const updateFood = async (req, res) => {
+    const { foodId } = req.params;
+    const updatedFood = req.body;
+    try {
+        if (!mongoose_1.default.Types.ObjectId.isValid(foodId)) {
+            res.status(404).json({ error: "Not Found" });
+            return;
+        }
+        const updatedFoodDocument = await foodSchema_1.default.findOneAndUpdate({ _id: foodId }, { $set: updatedFood }, { new: true });
+        if (!updatedFoodDocument) {
+            res.status(404).json({ error: "Food not found" });
+            return;
+        }
+        res.status(200).json(updatedFoodDocument);
+    }
+    catch (error) {
+        console.log("Food Update Error: ", error);
+    }
+};
+exports.updateFood = updateFood;
 //# sourceMappingURL=foodsController.js.map
