5a34fd4860caa08fbbfa08e5e3433ba10437649a
kngval <bkvalteocs@gmail.com>
2024-01-02 23:11:46 +0800

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import  { BrowserRouter, Route,Routes } from "react-router-dom"
+import Home from "./pages/Home"
+
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+          </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
