cbded4c283c603995cd4d6353f1175405cb60f85
kngval <bkvalteocs@gmail.com>
2024-01-02 23:14:27 +0800

    init twcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // JavaScript and TypeScript files
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
