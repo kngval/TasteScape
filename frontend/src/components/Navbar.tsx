21952c10203a90b91a49d6a83516e4262647a4ee
kngval <bkvalteocs@gmail.com>
2024-01-04 21:10:10 +0800

    removed hamburger

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 
-import userSvg  from "../assets/svgs/user-circle-svgrepo-com.svg";
+import userSvg  from "../assets/svgs/userIcon.svg";
 const Navbar:React.FC = () =>{
 
 
@@ -9,9 +9,7 @@ const Navbar:React.FC = () =>{
             <div className="container justify-between flex items-center mx-5">
                 <div className="flex items-center">
                 <button>
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-                </svg>
+              
             </button>
             <h1>TASTESCAPE</h1>
 
