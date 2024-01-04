3de1c20ed5e1897ce68f29d36b160cb3a254346e
kngval <bkvalteocs@gmail.com>
2024-01-04 16:02:26 +0800

    fixed hamburger, added user icon

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,23 @@
 
+import userSvg  from "../assets/svgs/user-circle-svgrepo-com.svg";
 const Navbar:React.FC = () =>{
 
 
     return(
         <>
-        <nav className="w-full p-7 shadow-md">
-            <div className="container justify-between flex items-center">
-            <h1>TASTESCAPE</h1>
-            <button className="border-2 p-2">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
+        <nav className="flex justify-center w-full p-5 shadow-md">
+            <div className="container justify-between flex items-center mx-5">
+                <div className="flex items-center">
+                <button>
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                </svg>
             </button>
+            <h1>TASTESCAPE</h1>
+
+        
+            </div>
+            <img className="w-7" src={userSvg} alt="" />
             </div>
         </nav>
         </>
