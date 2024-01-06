662b6d83bc8edc8d821d6dafc02e91b9f5155e56
kngval <bkvalteocs@gmail.com>
2024-01-06 21:23:09 +0800

    fixed fetching

--- backend/src/controllers/foodsController.ts
+++ backend/src/controllers/foodsController.ts
@@ -1,7 +1,23 @@
 import { Request, Response } from "express";
-import FoodModel, {Food} from "../models/foodSchema";
 import mongoose from "mongoose";
+import axios from "axios";
+import FoodModel, { Food } from "../models/foodSchema";
+import "dotenv/config";
 
+//FOR SEARCHING RECIPES
+export const getSearchList = async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`
+    https://api.edamam.com/search?q=${req.params.query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}
+    `);
+    console.log(response.data)
+    res.status(200).json(response.data.hits);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//FOR CREATING YOUR OWN RECIPES
 export const getFoodList = async (req: Request, res: Response) => {
   const foodList = await FoodModel.find().sort({ createdAt: -1 });
   res.status(200).json(foodList);
@@ -23,28 +39,25 @@ export const createFood = async (req: Request, res: Response) => {
 
 //update food interface
 
-
-
-export const updateFood = async (req : Request,res: Response) => {
+export const updateFood = async (req: Request, res: Response) => {
   const { foodId } = req.params;
-  const updatedFood: Partial<Food> = req.body
+  const updatedFood: Partial<Food> = req.body;
   try {
     if (!mongoose.Types.ObjectId.isValid(foodId)) {
       res.status(404).json({ error: "Not Found" });
-      return
+      return;
     }
 
     const updatedFoodDocument = await FoodModel.findOneAndUpdate(
-        { _id: foodId},
-        {$set: updatedFood},
-        {new : true}
+      { _id: foodId },
+      { $set: updatedFood },
+      { new: true }
     );
     if (!updatedFoodDocument) {
-        res.status(404).json({ error: "Food not found" });
-        return;
-      }
+      res.status(404).json({ error: "Food not found" });
+      return;
+    }
     res.status(200).json(updatedFoodDocument);
-
   } catch (error) {
     console.log("Food Update Error: ", error);
   }
