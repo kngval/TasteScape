695f112ff0663c17ed0e2a08cf2dbdc7eda1f35d
kngval <bkvalteocs@gmail.com>
2024-01-08 17:54:48 +0800

    ui changes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,14 @@
 
 //REACT
 import { useState,useEffect, FormEvent } from "react";
-import axios from "axios"
+// import axios from "axios"
 import IRecipes from "../interfaces/IRecipes"
 
 //Components
 import { HomeSlider } from "../components/HomeSlider";
 import Navbar from "../components/Navbar";
 import { Recipes } from "../components/Recipes";
+import { MockData } from "../mock/MockData"
 //assets
 import searchIcon from "../assets/svgs/search.svg"
 
@@ -21,9 +22,9 @@ const Home: React.FC = () => {
   const getRecipes = async() =>{
     try {
       
-      const response = await axios.get(`http://localhost:3000/Home/${query}`)
+      // const response = await axios.get(`http://localhost:3000/Home/${query}`)
       
-      const data = response.data
+      const data = MockData
 
       setRecipes(data)
       console.log(data);
@@ -53,7 +54,7 @@ const Home: React.FC = () => {
       <HomeSlider />
 
       {/* SEARCH BAR */}
-      <form onSubmit={handleSearch} className="w-full my-20 flex justify-center items-center">
+      <form onSubmit={handleSearch} className="w-full my-5 flex justify-center items-center text-xs">
 
        
         <input type="text" value={search} className="w-40 md:w-auto lg:w-1/3 h-8 p-4 rounded-s-full bg-gray-200 outline-none" onChange={(e) => setSearch(e.target.value)}/>
@@ -62,11 +63,11 @@ const Home: React.FC = () => {
         </button>
 
       
-        <div onClick={() => toggleDropdown(!dropdown)} className="w-20 md:w-1/5 lg:w-2/12 position: relative filter-container ml-4 p-2 rounded-full ">
+        <div onClick={() => toggleDropdown(!dropdown)} className="w-20 md:w-1/5 lg:w-2/12 position: relative filter-container ml-4 p-1 rounded-full ">
 
-        <button className="ml-2 text-xs" >Filter By : </button>        
+        <button className="ml-2 " >Filter By : </button>        
         {dropdown && 
-        <div className="w-full text-xs position: absolute left-0 top-12 dropdown p-4 rounded-md ">
+        <div className="w-full  position: absolute left-0 top-12 dropdown p-4 rounded-md ">
           <ul>
             <li>Filter 1</li>
             <li>Filter 2</li>
@@ -81,11 +82,15 @@ const Home: React.FC = () => {
 
         {/* RECIPES */}
 
+        <div className="recipe-container grid grid-cols-2 md:grid-cols-3 p-4 gap-5 text-xs">
        {recipes && recipes.length > 0 ? recipes.map((recipe) => (
-        <Recipes recipe={recipe.recipe}/>
+        <Recipes key={recipe.recipe.id} recipe={recipe.recipe}/>
        )) : <h1>No recipes</h1>
       }
 
+      {!recipes && <h1>No Recipes</h1>}
+
+      </div>
     </>
   );
 };
