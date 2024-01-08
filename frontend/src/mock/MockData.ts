750b2587d1ba8a997cc70e401ddbc8b097a2ccbd
kngval <bkvalteocs@gmail.com>
2024-01-08 17:55:03 +0800

    s

---
+++ frontend/src/mock/MockData.ts
@@ -0,0 +1,45 @@
+import IRecipes from "../interfaces/IRecipes";
+
+export const MockData: IRecipes[] = [
+  {
+    recipe: {
+      id: 1,
+      label: "Chicken Adobo",
+      image:
+        "https://www.eatingwell.com/thmb/YrI10pp9V6quKr98nscc_XFQ02g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/5147351-a7e0a28fde814b40bdcc67075e933b96.jpg",
+      ingredientLines: ["Soy Sauce", "Vinegar", "Chicken", "Basil Leaves"],
+    },
+  },
+
+  {
+    recipe: {
+      id: 2,
+      label: "Chicken Adobo",
+      image:
+        "https://www.eatingwell.com/thmb/YrI10pp9V6quKr98nscc_XFQ02g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/5147351-a7e0a28fde814b40bdcc67075e933b96.jpg",
+      ingredientLines: ["Soy Sauce", "Vinegar", "Chicken", "Basil Leaves"],
+    },
+  },
+
+  {
+    recipe: {
+      id: 3,
+
+      label: "Chicken Adobo",
+      image:
+        "https://www.eatingwell.com/thmb/YrI10pp9V6quKr98nscc_XFQ02g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/5147351-a7e0a28fde814b40bdcc67075e933b96.jpg",
+      ingredientLines: ["Soy Sauce", "Vinegar", "Chicken", "Basil Leaves"],
+    },
+  },
+
+  {
+    recipe: {
+      id: 4,
+
+      label: "Chicken Adobo",
+      image:
+        "https://www.eatingwell.com/thmb/YrI10pp9V6quKr98nscc_XFQ02g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/5147351-a7e0a28fde814b40bdcc67075e933b96.jpg",
+      ingredientLines: ["Soy Sauce", "Vinegar", "Chicken", "Basil Leaves"],
+    },
+  },
+];
