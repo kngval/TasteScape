3ddcd16e7c0204a0ded8315f0444debbe4aff7ca
kngval <bkvalteocs@gmail.com>
2024-01-08 17:53:50 +0800

    ui changes

--- frontend/src/components/Recipes.tsx
+++ frontend/src/components/Recipes.tsx
@@ -1,15 +1,18 @@
 import IRecipes from "../interfaces/IRecipes"
-
+import heart from "../assets/svgs/heart.svg"
 export const Recipes:React.FC<IRecipes> = ({recipe}) =>{
 
 
     return(
         <>
-            <div>
-            <h1>{recipe.label}</h1>
+            <div className="flex flex-col justify-center">
 
-            <img src={recipe.image} alt="" />
-            <p>{recipe.ingredientLines}</p>
+            <img src={recipe.image} className="food-img rounded-lg w-full h-28 sm:h-3/5 object-cover object-center" alt="" />
+
+            <div className="foodName flex justify-between mt-2 px-2 w-full">
+                <h1 className="">{recipe.label}</h1>
+                <img src={heart} className="w-3" alt="" />
+            </div>
             </div>
         </>
 
