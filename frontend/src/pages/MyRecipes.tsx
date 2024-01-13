a66d49f2989b7089a150d4f351a40dc9d1171bd3
kngval <bkvalteocs@gmail.com>
2024-01-13 13:49:18 +0800

    My Recipes Page

---
+++ frontend/src/pages/MyRecipes.tsx
@@ -0,0 +1,13 @@
+import { BottomNavbar } from "../components/BottomNavbar";
+
+const MyRecipes = () => {
+  return (
+    <>
+      <h1>RECIPE PAGE</h1>
+
+      <BottomNavbar />
+    </>
+  );
+};
+
+export default MyRecipes;
