39813c5b6fd893c318960ab90c2d4041b79fde0e
kngval <bkvalteocs@gmail.com>
2024-01-15 11:23:03 +0800

    profile page

---
+++ frontend/src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import { BottomNavbar } from "../components/BottomNavbar";
+
+const Profile = () => {
+  return (
+    <>
+        PROFILE
+        <BottomNavbar />
+    </>
+  );
+};
+
+export default Profile;
