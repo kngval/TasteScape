7bb3f9d155483883e047e29fda13f412e1e7556a
kngval <bkvalteocs@gmail.com>
2024-01-15 11:22:43 +0800

    likes page

---
+++ frontend/src/pages/LikedRecipes.tsx
@@ -0,0 +1,15 @@
+import { BottomNavbar } from "../components/BottomNavbar";
+
+const LikedRecipes = () =>{
+
+
+    return(
+        <>
+            LIKED RECIPES
+
+            <BottomNavbar />
+        </>
+    )
+}
+
+export default LikedRecipes;
