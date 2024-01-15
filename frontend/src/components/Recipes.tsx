c7b1f7a264141bf4e691e6640e2a22d109fa9990
kngval <bkvalteocs@gmail.com>
2024-01-15 11:21:58 +0800

    types

--- frontend/src/components/Recipes.tsx
+++ frontend/src/components/Recipes.tsx
@@ -1,21 +1,20 @@
-import IRecipes from "../interfaces/IRecipes"
-import heart from "../assets/svgs/heart.svg"
-export const Recipes:React.FC<IRecipes> = ({recipe}) =>{
+import IRecipes from "../interfaces/IRecipes";
+import heart from "../assets/svgs/heart.svg";
+export const Recipes: React.FC<IRecipes> = ({ recipe }) => {
+  return (
+    <>
+      <div className="flex flex-col justify-center">
+        <img
+          src={recipe.image}
+          className="food-img rounded-lg w-full h-28 sm:h-3/5 object-cover object-center"
+          alt=""
+        />
 
-
-    return(
-        <>
-            <div className="flex flex-col justify-center">
-
-            <img src={recipe.image} className="food-img rounded-lg w-full h-28 sm:h-3/5 object-cover object-center" alt="" />
-
-            <div className="foodName flex justify-between mt-2 px-2 w-full">
-                <h1 className="">{recipe.label}</h1>
-                <img src={heart} className="w-3" alt="" />
-            </div>
-            </div>
-        </>
-
-
-    )
-}
+        <div className="foodName flex justify-between mt-2 px-2 w-full">
+          <h1 className="">{recipe.label}</h1>
+          <img src={heart} className="w-3" alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
