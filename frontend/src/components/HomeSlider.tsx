a6c11e3a461c29f80df5f354bd331e1df92fccf6
kngval <bkvalteocs@gmail.com>
2024-01-16 14:36:25 +0800

    added padding for whitespace

--- frontend/src/components/HomeSlider.tsx
+++ frontend/src/components/HomeSlider.tsx
@@ -3,8 +3,8 @@ import ghibli1 from "../assets/images/ghibli1.jpg";
 export const HomeSlider: React.FC = () => {
   return (
     <>
-      <div className="img-container mx-4 my-10">
-        <img src={ghibli1} className="w-full h-full object-cover   rounded-xl" alt="" />
+      <div className="img-container mx-4 my-5 lg:p-10">
+        <img src={ghibli1} className="w-full  h-full object-cover rounded-xl" alt="" />
       </div>     
     </>
   );
