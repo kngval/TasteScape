3509b1c9e757f36fd76bb5967d81e2faccffc6c6
kngval <bkvalteocs@gmail.com>
2024-01-16 14:40:29 +0800

    responsiveness

--- frontend/src/components/SearchForm.tsx
+++ frontend/src/components/SearchForm.tsx
@@ -1,31 +1,19 @@
 import { useState,useEffect, FormEvent } from "react";
 // import axios from "axios"
-import { MockData } from "../mock/MockData"
 import searchIcon from "../assets/svgs/search.svg"
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../Redux/store";
+import { fetchRecipes } from "../Redux/foodSlice";
 
 const SearchForm = () => {
     const [dropdown, toggleDropdown] = useState(false)
    const [search, setSearch] = useState('');
    const [query, setQuery] = useState('');
-
-  const getRecipes = async() =>{
-    try {
-      
-      // const response = await axios.get(`http://localhost:3000/Home/${query}`)
-      
-      const data = MockData
-
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-      
-    }
-    
-  }
-
+   const dispatch = useDispatch<AppDispatch>()
+  
   useEffect(() => {
     if(query){
-      getRecipes()
+      dispatch(fetchRecipes())
     }
   },[query])
 
@@ -37,18 +25,15 @@ const SearchForm = () => {
   }
 
   return (
-    <form onSubmit={handleSearch} className="w-full my-5 flex justify-center items-center text-xs">
+    <form onSubmit={handleSearch} className="w-full lg:w-1/2  flex justify-center items-center text-xs">
 
        
-        <input type="text" value={search} className="w-40 md:w-auto lg:w-1/3 h-8 p-4 rounded-s-full bg-gray-200 outline-none" placeholder="Search for A Recipe..." onChange={(e) => setSearch(e.target.value)}/>
-        <button type="submit" className="w-10 flex items-center h-8 p-3 rounded-e-full bg-gray-200">
+        <input type="text" value={search} className="w-40 lg:w-full h-8 p-4 rounded-s-full bg-gray-200 outline-none" placeholder="Search for A Recipe..." onChange={(e) => setSearch(e.target.value)}/>
+        <button  type="submit" className="w-10 flex items-center h-8 p-3 rounded-e-full bg-gray-200">
           <img src={searchIcon} className="" alt="" />
         </button>
 
-
-      
         <div onClick={() => toggleDropdown(!dropdown)} className="lg:hidden w-20 md:w-1/5 lg:w-2/12 position: relative filter-container ml-4 p-1 rounded-full ">
-
         <button className="ml-2 " >Filter By : </button>        
         {dropdown && 
         <div className="w-full  position: absolute left-0 top-12 dropdown p-4 rounded-md ">
