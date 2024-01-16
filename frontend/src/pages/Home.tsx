c8ff092ccf5f44ebe8250c1198ff037e2c715981
kngval <bkvalteocs@gmail.com>
2024-01-16 14:38:37 +0800

    fixed redux fetch

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,42 +1,47 @@
 
-//REACT
-
+//REACT & REDUX
+import { RootState } from '../Redux/store'
+import { useSelector } from 'react-redux';
 
 //Components
 import { HomeSlider } from "../components/HomeSlider";
 import Navbar from "../components/Navbar";
-// import { Recipes } from "../components/Recipes";
 import SideFilter from "../components/SideFilter";
 import { BottomNavbar } from "../components/BottomNavbar";
-//assets
-// import chef from "../assets/svgs/chef.svg"
+import { Recipes } from '../components/Recipes';
+import SearchForm from '../components/SearchForm';
 
+//assets
+import recipe from "../assets/svgs/recipe.svg"
 const Home: React.FC = () => {
+   const recipes = useSelector((state: RootState) => state.recipes.recipes)
    
+
   return (
     <>
       <Navbar />
       <HomeSlider />
-
       {/* SEARCH BAR */}
-
-      <div className="flex">
+      <div className="flex lg:px-20"> 
         <SideFilter />
-      
-        </div>
-        {/* RECIPES */}
 
-        {/* <div className={`text-center recipe-container flex-grow ${recipes.length === 0 ? 'flex justify-center' : 'grid'} grid-cols-2 mb-20 md:grid-cols-3 p-4 gap-5 text-xs`}>
-       {recipes && recipes.length > 0 ? recipes.map((recipe) => (
-         <Recipes key={recipe.recipe.id} recipe={recipe.recipe}/>
-         )) : (
-          <div className="mt-10">
-            <img src={chef} className="" alt="" />
-            <h1 className="text-gray-300 text-xl">No Recipes</h1>
+        {/* RECIPES */}
+          <div className='w-full flex flex-col items-center'>
+        <SearchForm />
+        <div className={`recipes  text-center recipe-container  ${recipes.length === 0 ? 'flex justify-center' : 'grid'} grid-cols-2 mb-20  md:grid-cols-3 p-5 gap-5 text-xs `}>
+
+          {recipes && recipes.length !== 0 && recipes.map((recipe,index) => (
+            <Recipes key={index} recipe={recipe.recipe} />
+          ))}
+          {!recipes || recipes.length === 0 && 
+            <div className='text-gray-400 flex flex-col'>
+              <img src={recipe} alt="" />
+              <h1>Discover Recipes</h1>
+            </div>
+          }
          </div>
-         )
-        }
-      </div> */}
+         </div>
+         </div>
 
           <BottomNavbar />
     </>
