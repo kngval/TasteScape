191ebbe1f579e3b03224764f6340a67024faa930
kngval <bkvalteocs@gmail.com>
2024-01-16 14:35:20 +0800

    ui changes

--- frontend/src/components/SideFilter.tsx
+++ frontend/src/components/SideFilter.tsx
@@ -1,14 +1,31 @@
-
 const SideFilter = () => {
   return (
-    <div className="side-filter h-2/5  hidden lg:block">
-        <ul>
-            <li>asd</li>
-            <li>sad</li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
+    <div className="min-w-72 mr-10 rounded-2xl bg-customWhite p-6 side-filter h-2/5  hidden lg:block">
+      <h1 className="text-2xl font-black">Filter By: </h1>
+      <ul className="text-xl">
+        <li>asd</li>
+      </ul>
+      <ul>
+        <li>asd</li>
+        <li>sad</li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ul>
+        <li>asd</li>
+        <li>sad</li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <ul>
+        <li>asd</li>
+        <li>sad</li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 };
