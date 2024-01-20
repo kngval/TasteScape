fe064f2bfaa528e8e6e1fd84d3bcebb0b8f2c0c5
kngval <bkvalteocs@gmail.com>
2024-01-20 20:33:22 +0800

    added new samples for mock data

--- frontend/src/mock/MockData.ts
+++ frontend/src/mock/MockData.ts
@@ -3,7 +3,7 @@ import IRecipes from "../interfaces/IRecipes";
 export const MockData: IRecipes[] = [
   {
     recipe: {
-      id: 1,
+      uri: "food1",
       label: "Chicken Adobo",
       image:
         "https://www.eatingwell.com/thmb/YrI10pp9V6quKr98nscc_XFQ02g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/5147351-a7e0a28fde814b40bdcc67075e933b96.jpg",
@@ -13,7 +13,7 @@ export const MockData: IRecipes[] = [
 
   {
     recipe: {
-      id: 2,
+      uri: "food2",
       label: "Cordon Bleu",
       image:
         "https://staticcookist.akamaized.net/wp-content/uploads/sites/22/2021/09/Chicken-Cordon-Bleu-Roll-Ups-1200x675.jpeg",
@@ -23,8 +23,7 @@ export const MockData: IRecipes[] = [
 
   {
     recipe: {
-      id: 3,
-
+      uri: "food3",
       label: "Chicken Katsu Curry",
       image:
         "https://takestwoeggs.com/wp-content/uploads/2021/09/Air-Fryer-Chicken-Katsu-curry-takestwoeggs-FINAL-SQ.jpg",
@@ -34,8 +33,7 @@ export const MockData: IRecipes[] = [
 
   {
     recipe: {
-      id: 4,
-
+      uri: "food4",
       label: "Chicken Adobo",
       image:
         "https://www.eatingwell.com/thmb/YrI10pp9V6quKr98nscc_XFQ02g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/5147351-a7e0a28fde814b40bdcc67075e933b96.jpg",
