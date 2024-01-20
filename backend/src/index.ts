6d489a37aa5184f866ef3fa8c9d94b3c5190192e
kngval <bkvalteocs@gmail.com>
2024-01-20 20:05:55 +0800

    server bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,29 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import foodRoutes from "./routes/foodRoutes"
-import foodSearch from "./routes/foodSearch"
+import foodRoutes from "./routes/foodRoutes";
+import foodSearch from "./routes/foodSearch";
 require("dotenv").config();
 const app = express();
 
 //middleware
 app.use(express.json());
 app.use(cors());
-app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend's origin
-  credentials: true,
-}));app.use('/profile', foodRoutes)
-app.use('/Home', foodSearch)
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend's origin
+    credentials: true,
+  })
+);
+app.use("/profile", foodRoutes);
+app.use("/Home", foodSearch);
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() =>
     app.listen(process.env.PORT, () => console.log("Server listening..."))
-  ).catch((error : Error) => console.log(error));
+  )
+  .catch((error: Error) => console.log(error));
 
 mongoose.connection.on("error", (error: Error) => {
   console.log("MongoDB Connection Error : ", error);
