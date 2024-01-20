348bbce605bba65532dd9089ec9a01ae812a29c1
kngval <bkvalteocs@gmail.com>
2024-01-20 20:30:39 +0800

    added link to specific recipe

--- frontend/src/components/Recipes.tsx
+++ frontend/src/components/Recipes.tsx
@@ -1,14 +1,26 @@
 import IRecipes from "../interfaces/IRecipes";
 import heart from "../assets/svgs/heart.svg";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../Redux/store";
+import { setChosenRecipe } from "../Redux/chosenRecipeSlice";
 export const Recipes: React.FC<IRecipes> = ({ recipe }) => {
+  const dispatch = useDispatch<AppDispatch>()
+  
+  const handleClick = (chosenRecipe: IRecipes) =>{
+    dispatch(setChosenRecipe({recipe : chosenRecipe.recipe}))
+  }
+
   return (
     <>
-      <div className="flex  flex-col justify-center">
+       <div className="" >
+        <Link to={`/recipe/${encodeURIComponent(recipe.uri)}`} onClick={() => handleClick({recipe})}>
         <img
           src={recipe.image}
           className="food-img rounded-lg w-full h-28 sm:h-3/5 object-cover "
           alt=""
         />
+        </Link> 
 
         <div className="foodName flex justify-between mt-2 px-2 w-full">
           <h1 className="">{recipe.label}</h1>
