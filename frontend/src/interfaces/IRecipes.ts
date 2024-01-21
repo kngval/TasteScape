e2d89d07c9870e67691558474cdbe0aca2479a67
kngval <bkvalteocs@gmail.com>
2024-01-21 18:59:35 +0800

    recipe structure

--- frontend/src/interfaces/IRecipes.ts
+++ frontend/src/interfaces/IRecipes.ts
@@ -1,8 +1,15 @@
+
+
+
 interface Recipe {
         recipe : {
             uri: string,
             label : string,
             image: string,
+            cuisineType: string,
+            calories: number,
+            yield: number,
+            totalTime: number,
             ingredientLines: string[]
         }
 }
