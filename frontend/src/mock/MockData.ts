7982cc9582bc143466788c1727387b6081449154
kngval <bkvalteocs@gmail.com>
2024-01-21 19:01:11 +0800

    new structure for mock data

--- frontend/src/mock/MockData.ts
+++ frontend/src/mock/MockData.ts
@@ -4,7 +4,11 @@ export const MockData: IRecipes[] = [
   {
     recipe: {
       uri: "food1",
-      label: "Chicken Adobo",
+      label: "Filipino Chicken Adobo",
+      cuisineType: "Filipino",
+      calories: 70,
+      yield: 7,
+      totalTime: 50,
       image:
         "https://www.eatingwell.com/thmb/YrI10pp9V6quKr98nscc_XFQ02g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/5147351-a7e0a28fde814b40bdcc67075e933b96.jpg",
       ingredientLines: ["Soy Sauce", "Vinegar", "Chicken", "Basil Leaves"],
@@ -15,6 +19,10 @@ export const MockData: IRecipes[] = [
     recipe: {
       uri: "food2",
       label: "Cordon Bleu",
+      cuisineType: "Switzerland",
+      calories: 40,
+      yield: 10,
+      totalTime: 80,
       image:
         "https://staticcookist.akamaized.net/wp-content/uploads/sites/22/2021/09/Chicken-Cordon-Bleu-Roll-Ups-1200x675.jpeg",
       ingredientLines: ["Pagmamahal", "Aruga", "Kiss", "Cuddle"],
@@ -25,6 +33,10 @@ export const MockData: IRecipes[] = [
     recipe: {
       uri: "food3",
       label: "Chicken Katsu Curry",
+      cuisineType: "Japanese",
+      calories: 50,
+      yield: 7,
+      totalTime: 50,
       image:
         "https://takestwoeggs.com/wp-content/uploads/2021/09/Air-Fryer-Chicken-Katsu-curry-takestwoeggs-FINAL-SQ.jpg",
       ingredientLines: ["Soy Sauce", "Vinegar", "Chicken", "Basil Leaves"],
@@ -34,9 +46,13 @@ export const MockData: IRecipes[] = [
   {
     recipe: {
       uri: "food4",
-      label: "Chicken Adobo",
+      label: "Aglio e Olio",
+      cuisineType: "Italian",
+      calories: 40,
+      yield: 4,
+      totalTime: 60,
       image:
-        "https://www.eatingwell.com/thmb/YrI10pp9V6quKr98nscc_XFQ02g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/5147351-a7e0a28fde814b40bdcc67075e933b96.jpg",
+        "https://walkingthroughlavenderfields.com/wp-content/uploads/2022/09/aglio-e-olio-e-peperoncino-01.jpeg",
       ingredientLines: ["Soy Sauce", "Vinegar", "Chicken", "Basil Leaves"],
     },
   },
