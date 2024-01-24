6ee62f7c6367330b07e21eff87459deeb7c829a2
kngval <bkvalteocs@gmail.com>
2024-01-24 15:31:40 +0800

    mock data fetching

--- frontend/src/Redux/chosenRecipeSlice.ts
+++ frontend/src/Redux/chosenRecipeSlice.ts
@@ -1,51 +1,74 @@
-import {  createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from 'axios'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { FoodMock } from '../mock/foodMockData'
+// import axios from "axios";
 export interface RecipeDetails {
-  id: number,
-  title: string,
-  image: string,
-  servings: number,
+  id: number;
+  title: string;
+  image: string;
+  servings: number;
   readyInMinutes: number;
-  cuisines: string[] | "N/A";
-  healthScore: number,
-  instructions: string,
-  extendedIngredients: [
-    {
+  cuisines: string[];
+  healthScore: number;
+  analyzedInstructions:{
+    steps:{
+      step:string
+    }[]
+  }
+  extendedIngredients:{
       original: string;
-    }
-  ];
-
+    }[];
 }
+
+
 
 interface ChosenRecipe {
   chosenRecipe: RecipeDetails | null;
+  loading: boolean;
+  error: string | null;
 }
 
 const initialState: ChosenRecipe = {
-  chosenRecipe: null,
+  chosenRecipe: FoodMock,
+  loading: true,
+  error: null,
 };
-export const fetchChosenRecipe = createAsyncThunk('recipe/fetchChosenRecipe', async(id:number) => {
-  try{
-    const response  = await axios.get(`http://localhost:3000/home/recipe/${id}`,{withCredentials:true})
-    console.log(response.data)
-    const data = response.data;
-    return data  as RecipeDetails;
-  } catch (error){
-    console.log(error)
+export const fetchChosenRecipe = createAsyncThunk(
+  "recipe/fetchChosenRecipe",
+  async () => {
+    try {
+      // const response = await axios.get(`http://localhost:3000/home/recipe/${id}`, {
+      //   withCredentials: true,
+      // });
+      // console.log(response.data);
+      // const data = response.data;
+      // return data as RecipeDetails;
+      
+      return FoodMock as RecipeDetails
+    } catch (error) {
+      console.log(error);
+    }
   }
-})
-
+);
 
 const chosenRecipeSlice = createSlice({
   name: "chosenRecipe",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchChosenRecipe.fulfilled, (state, action) => {
-      if (action.payload) {
-        state.chosenRecipe = action.payload;
-      }
-    });
+    builder
+      .addCase(fetchChosenRecipe.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchChosenRecipe.fulfilled, (state, action) => {
+        state.loading = false;
+        state.chosenRecipe = action.payload || null;
+      })
+      .addCase(fetchChosenRecipe.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message || "Error fetching recipe details";
+      });
   },
 });
+
 export default chosenRecipeSlice.reducer;
