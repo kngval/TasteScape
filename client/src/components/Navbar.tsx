bdd77cb48eb6a876c70e4b3481a62d46beda0a64
Kngval <bkvalteocs@gmail.com>
2024-01-26 19:05:37 +0800

    adjusted z-index and position, added nav links

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,16 +5,22 @@ const Navbar:React.FC = () =>{
 
     return(
         <>
-        <nav className="hidden justify-center w-full  lg:flex p-5 shadow-md ">
-            <div className="container justify-between flex items-center mx-5">
+        <nav className="hidden sticky top-0 justify-center w-full bg-white z-20 lg:flex p-5 shadow-md ">
+            <div className="container w-full justify-between flex items-center mx-5">
                 <div className="flex items-center">
                 <button>
               
             </button>
             <h1>TASTESCAPE</h1>
 
-        
+
             </div>
+                <div className="grid grid-cols-3 list-none gap-10">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Features</a></li>
+                    <li><a href="">Create</a></li>
+                    
+                </div>
             <img className="w-7" src={userSvg} alt="" />
             </div>
         </nav>
