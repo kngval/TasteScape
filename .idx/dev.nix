4e69041e92980c0e9b7c4cb0e9e8786164a8fe71
brentvalino61 <brentvalino61@gmail.com>
2024-01-28 09:48:41 +0000

    idxconfig

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -10,7 +10,9 @@
 
   # Sets environment variables in the workspace
   env = {
-    SOME_ENV_VAR = "hello";
+    PORT = 3000;
+    MONGO_URI = "mongodb+srv://kngval:kngvaljavascript21@tastescape.imeeypd.mongodb.net/?retryWrites=true&w=majority";
+    API_KEY="e2af5c6d5b774fdcbe4ad7ad852099d8";
   };
 
   # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
