f1f035e79ac76c35254471f34d19a09429c502c6
brentvalino61 <brentvalino61@gmail.com>
2024-01-28 08:06:55 +0000

    responsive

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,8 +5,8 @@ const Navbar:React.FC = () =>{
 
     return(
         <>
-        <nav className="hidden sticky top-0 justify-center w-full bg-white z-20 lg:flex p-5 shadow-md ">
-            <div className="container w-full justify-between flex items-center mx-5">
+        <nav className="text-sm lg:text-md sticky top-0 flex justify-center w-full bg-white p-4 shadow-md z-20">
+            <div className="container w-10/12 justify-between flex items-center mx-5">
                 <div className="flex items-center">
                 <button>
               
@@ -15,8 +15,8 @@ const Navbar:React.FC = () =>{
 
 
             </div>
-                <div className="grid grid-cols-3 list-none gap-10">
-                    <li><a href="">Home</a></li>
+                <div className="hidden lg:grid grid-cols-3 list-none gap-10">
+                    <li><a href="/home">Home</a></li>
                     <li><a href="">Features</a></li>
                     <li><a href="">Create</a></li>
                     
