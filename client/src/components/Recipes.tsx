b378b368295abdf0e6be7245076c3fcd4bd219e5
Kngval <bkvalteocs@gmail.com>
2024-01-29 16:41:43 +0800

    like option

--- client/src/components/Recipes.tsx
+++ client/src/components/Recipes.tsx
@@ -1,10 +1,21 @@
 import IRecipes from "../interfaces/IRecipes";
 import heart from "../assets/svgs/heart.svg";
 import { Link } from "react-router-dom";
-
+import { AppDispatch } from "../Redux/store";
+import { addLikedRecipe } from "../Redux/likedRecipeSlice";
+import { useDispatch } from "react-redux";
 
 export const Recipes: React.FC<IRecipes> = ({ image , title, id }) => {
-  
+    const dispatch = useDispatch<AppDispatch>()
+
+  const handleClick = () => {
+    dispatch(addLikedRecipe({
+      image,
+      title,
+      id
+    }))
+  }
+
   return (
     <>
       <div className="md:text-lg">
@@ -22,7 +33,7 @@ export const Recipes: React.FC<IRecipes> = ({ image , title, id }) => {
           <div className="w-10/12 text-start">
           <h1 className="">{title}</h1>
           </div>
-          <img src={heart} className="w-3 md:w-5" alt="" onClick={() => alert('hello')}/>
+          <img src={heart} className="w-3 md:w-5" alt="" onClick={() => handleClick()}/>
         </div>
         </div>
     </>
