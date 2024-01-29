b0e79fef7783dcdf2dfc0abc6e3d475e4ebfc26a
Kngval <bkvalteocs@gmail.com>
2024-01-29 16:40:16 +0800

    fixed fetching liked recipes

---
+++ client/src/Redux/likedRecipeSlice.ts
@@ -0,0 +1,67 @@
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios"
+import IRecipes from "../interfaces/IRecipes";
+
+interface LikedRecipes {
+    likedRecipes: IRecipes[] | null
+    loading : boolean
+    error: string | null
+}
+
+const initialState:LikedRecipes = {
+    likedRecipes: null,
+    loading: true,
+    error: null
+}
+
+export const addLikedRecipe = createAsyncThunk('recipe/addLikedRecipe', async({id,image,title}:IRecipes,thunkAPI) => {
+    try{
+        const response  = await axios.post("http://localhost:3000/home",{id,image,title});
+        console.log(response);
+        
+        return response
+    }catch(error){
+        return thunkAPI.rejectWithValue(error);
+    }
+
+})
+
+export const fetchLikedRecipes = createAsyncThunk('recipe/fetchLikedRecipe',async() => {
+    try{
+        const response = await axios.get(`http://localhost:3000/liked-recipes`)
+
+        const data = response.data
+        console.log(data);
+        return data as IRecipes[]
+
+    } catch(error){
+
+    }
+})
+
+const LikedRecipeSlice = createSlice({
+    name: "likedRecipes",
+    initialState,
+    reducers:{
+        displayLikedRecipes: (state,action:PayloadAction<IRecipes[]>) => {
+            state.likedRecipes = [...action.payload]
+        }
+    },
+    extraReducers: (builder) => {
+        builder
+          .addCase(fetchLikedRecipes.pending, (state) => {
+            state.loading = true;
+          })
+          .addCase(fetchLikedRecipes.fulfilled, (state, action) => {
+            state.loading = false;
+            state.likedRecipes = action.payload || null;
+          })
+          .addCase(fetchLikedRecipes.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message ?? "An error occurred.";
+          });
+      },
+    
+})
+export default LikedRecipeSlice.reducer;
+export const { displayLikedRecipes } = LikedRecipeSlice.actions
