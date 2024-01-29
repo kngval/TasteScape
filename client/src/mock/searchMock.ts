3a4c76d73fdc7f123e4b4165bcfa53bf3ac563f1
Kngval <bkvalteocs@gmail.com>
2024-01-29 16:43:06 +0800

    lRecipes

--- client/src/mock/searchMock.ts
+++ client/src/mock/searchMock.ts
@@ -2,21 +2,21 @@ export const searchMock = [
     {
         id:1,
         title: 'Filipino Chicken Adobo',
-        image: 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FEdit%2F2022-10-Chicken-Adobo%2FChicken_Adobo',
+        image: 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FEdit%2F2022-10-Chicken-Adobo%2FChicken_Adobo'
     },
     {
         id:2,
         title: 'Cordon Bleu',
-        image: 'https://www.simplyrecipes.com/thmb/G0WpDNn_iGeaSImHqH2TGiG8rjY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Spaghetti-Carbonara-LEAD-7-82d6bacf7f3848a4943b14712ab205ff.jpg',
+        image: 'https://www.simplyrecipes.com/thmb/G0WpDNn_iGeaSImHqH2TGiG8rjY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Spaghetti-Carbonara-LEAD-7-82d6bacf7f3848a4943b14712ab205ff.jpg'
     },
     {
         id:3,
         title: 'Filipino Chicken Adobo',
-        image: 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FEdit%2F2022-10-Chicken-Adobo%2FChicken_Adobo',
+        image: 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FEdit%2F2022-10-Chicken-Adobo%2FChicken_Adobo'
     },
     {
         id:4,
         title: 'Cordon Bleu',
-        image: 'https://www.simplyrecipes.com/thmb/G0WpDNn_iGeaSImHqH2TGiG8rjY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Spaghetti-Carbonara-LEAD-7-82d6bacf7f3848a4943b14712ab205ff.jpg',
+        image: 'https://www.simplyrecipes.com/thmb/G0WpDNn_iGeaSImHqH2TGiG8rjY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Spaghetti-Carbonara-LEAD-7-82d6bacf7f3848a4943b14712ab205ff.jpg'
     }
 ]
--- client/src/pages/LikedRecipes.tsx
+++ client/src/pages/LikedRecipes.tsx
@@ -1,11 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
 import { BottomNavbar } from "../components/BottomNavbar";
-
+import { AppDispatch, RootState } from "../Redux/store";
+import { useEffect } from "react";
+import { displayLikedRecipes, fetchLikedRecipes } from "../Redux/likedRecipeSlice";
+import { Recipes } from "../components/Recipes";
 const LikedRecipes = () =>{
-
+    const liked = useSelector((state:RootState) => state.likedRecipes.likedRecipes)
+    const dispatch = useDispatch<AppDispatch>()
+    useEffect(() => {
+        dispatch(fetchLikedRecipes())
+    },[])
 
     return(
         <>
             LIKED RECIPES
+            {liked && liked.map((likedRecipe) => (
+                <div>
+                    <Recipes id={likedRecipe.id} image={likedRecipe.image} title={likedRecipe.title}/>
+                </div>
+            ))}
 
             <BottomNavbar />
         </>
