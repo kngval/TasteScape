3ca5a88b1b2b6159e92820059990b5a8262c75be
Kngval <bkvalteocs@gmail.com>
2024-01-30 23:34:15 +0800

    'responsive'

--- client/src/components/Recipes.tsx
+++ client/src/components/Recipes.tsx
@@ -17,22 +17,22 @@ export const Recipes: React.FC<IRecipes> = ({ image , title, id }) => {
 
   return (
     <>
-      <div className="md:text-md xl:text-lg p-4 shadow-xl  rounded-lg">
+      <div className="md:text-md xl:text-lg  p-4 shadow-md  rounded-lg">
         <Link to={`/recipe/${id}`}>
        <div className="" >
         <img
           src={image}
-          className="food-img  w-full h-32 sm:h-48 lg:h-32 xl:h-52 2xl:h-64 object-cover"
+          className="food-img  w-full h-[120px] sm:h-48 lg:h-32 xl:h-42 2xl:h-60 object-cover"
           alt=""
         /> 
       </div>
         </Link>
 
-        <div className="foodName flex justify-between items-center mt-2 w-full">
-          <div className="w-10/12 text-start">
+        <div className="foodName flex justify-between items-center mt-2 ">
+          <div className="text-start">
           <h1 className="">{title}</h1>
           </div>
-          <svg onClick={handleClick} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 stroke-1 hover:fill-customPink hover:stroke-0">
+          <svg onClick={() => handleClick()} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={0.5} stroke="currentColor" className="ml-2 w-5 h-5 stroke-1 hover:fill-customPink hover:stroke-0">
   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
 </svg>
 
