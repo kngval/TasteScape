c98cc862d3308dbaee85066af3e21828c8fc40e0
Kngval <bkvalteocs@gmail.com>
2024-01-30 23:37:51 +0800

    'popup_modal_error_handling'

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 //REACT & REDUX
-import { RootState } from "../Redux/store";
-import { useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../Redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 //Components
 import { HomeSlider } from "../components/HomeSlider";
@@ -11,40 +12,75 @@ import { Recipes } from "../components/Recipes";
 import SearchForm from "../components/SearchForm";
 
 //assets
-import recipeImg from "../assets/svgs/recipe.svg";
-
-
+// import recipeImg from "../assets/svgs/recipe.svg";
+import { displayError } from "../Redux/likedRecipeSlice";
+import addedRecipe from "../assets/svgs/addedRecipe.svg";
+import chef from "../assets/svgs/chef.svg"
 const Home: React.FC = () => {
   const recipes = useSelector((state: RootState) => state.recipes.recipes);
+  // const dispatch = useDispatch<AppDispatch>()
+  const error = useSelector((state: RootState) => state.likedRecipes.error);
+  const [showError, setShowError] = useState<boolean>(false);
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    console.log(showError);
+    if (error) {
+      setShowError(true);
+
+      const timeout = setTimeout(() => {
+        setShowError(false);
+        dispatch(displayError(null));
+      }, 2000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [error]);
 
   return (
     <>
       <Navbar />
       <HomeSlider />
-      {/* SEARCH BAR */}
+      <div
+        className={`${
+          showError ? "fixed" : "hidden"
+        } flex justify-center items-center top-20 z-20 w-full h-20`}
+      >
+        <div className="flex items-center justify-center bg-gray-100 rounded-lg w-[250px] p-4 text-xs xl:text-lg xl:w-[400px]">
+          <img src={addedRecipe} className="h-10" alt="" />
+          <h1 className="ml-5 text-center ">{showError && error}</h1>
+        </div>
+      </div>
       <div className="flex lg:px-20">
         <SideFilter />
-
-        {/* RECIPES */}
         <div className="w-full flex flex-col items-center">
+          {/* SEARCH BAR */}
           <SearchForm />
           <div
             className={`recipes w-full text-center recipe-container  ${
               recipes.length === 0 ? "flex justify-center" : "grid"
-            } grid-cols-2 mb-20  md:grid-cols-3 p-5 gap-5 text-xs `}
+            } grid-cols-2 mb-20  md:grid-cols-3  p-5 gap-3 text-xs`}
           >
+            {/* RECIPES */}
             {recipes &&
               recipes.length !== 0 &&
-              recipes.map((recipe,) => (
-                
-                <Recipes key={recipe.id} id={recipe.id}  image={recipe.image} title={recipe.title} />
-                
+              recipes.map((recipe) => (
+                <Recipes
+                  key={recipe.id}
+                  id={recipe.id}
+                  image={recipe.image}
+                  title={recipe.title}
+                />
               ))}
             {!recipes ||
               (recipes.length === 0 && (
-                <div className="text-gray-400 flex flex-col">
-                  <img src={recipeImg} alt="" />
-                  <h1>Discover Recipes</h1>
+                <div className="w-full flex flex-col items-center">
+                  <img className="my-5 w-28
+                  " src={chef} alt="" />
+                  <p className="w-10/12 uppercase text-xxs sm:text-xs">Discover Vast Recipes from Around the World <span className="block">with</span></p>
+                  
+                  <h1 className="text-lg font-bold">TASTESCAPE</h1>
+                  
+
                 </div>
               ))}
           </div>
