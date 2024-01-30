d4cc41b7abc1c7eca6a252664bcc5a4e7e48aab4
Kngval <bkvalteocs@gmail.com>
2024-01-30 23:36:32 +0800

    'fixed_req_to_db'

--- server/src/controllers/foodsController.ts
+++ server/src/controllers/foodsController.ts
@@ -8,63 +8,74 @@ import "dotenv/config";
 //FOR SEARCHING RECIPES
 export const getSearchList = async (req: Request, res: Response) => {
   try {
-    const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.API_KEY}&query=${req.params.query}`)
-    const data = response.data.results
-    console.log(data)
+    const response = await axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.API_KEY}&query=${req.params.query}`
+    );
+    const data = response.data.results;
+    console.log(data);
     res.status(200).json(data);
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 // CONTROLLER FOR CLICKING A SPECIFIC RECIPE
-export const getRecipeDetails = async(req: Request, res: Response) => {
-  const  { id } = req.params
+export const getRecipeDetails = async (req: Request, res: Response) => {
+  const { id } = req.params;
   try {
-    const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.API_KEY}&includeNutrition=true`)
+    const response = await axios.get(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.API_KEY}&includeNutrition=true`
+    );
     const data = response.data;
     res.status(200).json(data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 //LIKING RECIPES
-export const addLikedRecipe = async(req:Request,res:Response) => {
-    const { id,title,image }:{id:string,title:string, image:string} = req.body
-    console.log('Received Data:', { id, title, image });
-    try{ 
+export const addLikedRecipe = async (req: Request, res: Response) => {
+  const { id, title, image }: { id: string; title: string; image: string } =
+    req.body;
+    console.log("Received Data:", { id, title, image });
+
+    
+    try {
+      const exists = await LikedModel.findOne({ id });
+      if (!exists) {
       const LikedRecipes = await LikedModel.create({
         id,
         title,
-        image
-      })
-      res.status(200).json(LikedRecipes)
-    } catch(error) {
-      res.status(400).json({error: error.message})
+        image,
+      });
+      res.status(200).json(LikedRecipes);
+    } else {
+      throw Error("Already Liked this Recipe");
     }
-}
-
-export const removeLikedRecipe = async(req:Request, res:Response) => {
-  const { id } = req.body;
-  try{
-    const removedRecipe = await LikedModel.deleteOne({ id });
-    res.status(200).json(removedRecipe)
-  }catch(error){
-    res.status(400).json({ error: error.message })
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({error : error.message})
   }
-}
+};
+
+export const removeLikedRecipe = async (req: Request, res: Response) => {
+  const { id } = req.body;
+  try {
+    const removedRecipe = await LikedModel.deleteOne({ id });
+    res.status(200).json(removedRecipe);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 //FETCH LIKED RECIPES
 
-  export const fetchLikedRecipe = async(req:Request,res:Response) => {
-    try{
-      const LikedRecipes  = await LikedModel.find().sort({ createdAt: -1 })
-      res.status(200).json(LikedRecipes)
-    }catch(error){
-
-    }
-  }
+export const fetchLikedRecipe = async (req: Request, res: Response) => {
+  try {
+    const LikedRecipes = await LikedModel.find().sort({ createdAt: -1 });
+    res.status(200).json(LikedRecipes);
+  } catch (error) {}
+};
 
 //FOR CREATING YOUR OWN RECIPES
 export const getFoodList = async (req: Request, res: Response) => {
