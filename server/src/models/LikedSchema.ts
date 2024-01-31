58d51fb582f2f8f97409f6f7023820b6b86077db
Kngval <bkvalteocs@gmail.com>
2024-01-31 16:35:35 +0800

    added new key value

--- server/src/models/LikedSchema.ts
+++ server/src/models/LikedSchema.ts
@@ -1,11 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const LikedSchema = new mongoose.Schema({
-        id: Number,
-        title: String,
-        image:String,
-        liked: Boolean
-})
+  id: Number,
+  title: String,
+  image: String,
+  isLiked: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-const LikedModel = mongoose.model<Document>('Liked Recipes', LikedSchema)
+const LikedModel = mongoose.model<Document>("Liked Recipes", LikedSchema);
 export default LikedModel;
