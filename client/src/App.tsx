823fdd290872e2f25fc966cdc0f24b3887fd0b72
Kngval <bkvalteocs@gmail.com>
2024-01-31 16:29:06 +0800

    navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,28 @@
-import  { BrowserRouter, Route,Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import MyRecipes from "./pages/MyRecipes"
-import LikedRecipes from "./pages/LikedRecipes"
-import Profile from "./pages/Profile"
-import RecipeDetails from "./pages/RecipeDetails"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import MyRecipes from "./pages/MyRecipes";
+import LikedRecipes from "./pages/LikedRecipes";
+import Profile from "./pages/Profile";
+import RecipeDetails from "./pages/RecipeDetails";
+import Navbar from "./components/Navbar";
+import { BottomNavbar } from "./components/BottomNavbar";
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-          <Routes>
-            <Route path="/home" element={<Home />}/>
-            <Route path="/my-recipes" element={<MyRecipes />}/>
-            <Route path="/liked-recipes" element={<LikedRecipes />}/>
-            <Route path="/profile" element={<Profile />}/>
-            <Route path="/recipe/:id" element={<RecipeDetails />}/>
-          </Routes>
-
-      </BrowserRouter>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/my-recipes" element={<MyRecipes />} />
+          <Route path="/liked-recipes" element={<LikedRecipes />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+        </div>
+        <BottomNavbar />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
