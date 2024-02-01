82ccb4e55ac4cd317159d03a416e01730387094e
brentvalino61 <brentvalino61@gmail.com>
2024-02-01 13:56:48 +0000

    fixed error and success msg

--- client/src/components/ErrorPopUp.tsx
+++ client/src/components/ErrorPopUp.tsx
@@ -1,35 +1,67 @@
-import { useDispatch, useSelector } from "react-redux"
-import { RootState, AppDispatch } from "../Redux/store"
-import { deleteLikedRecipe, displayError, displaySuccessMsg } from "../Redux/likedRecipeSlice";
-export const ErrorPopUp = ({id} : {id:number}) => {
-    const error = useSelector((state:RootState) => state.likedRecipes.error)
-    const dispatch = useDispatch<AppDispatch>();
-  const closePopUp =()=>{
-    dispatch(displayError(null))
-    dispatch(displaySuccessMsg(null))
-  }
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../Redux/store";
+import { displayError, displaySuccessMsg } from "../Redux/likedRecipeSlice";
+import addedRecipe from "../assets/svgs/addedRecipe.svg";
 
-  const handleDelete = () => {
-    dispatch(deleteLikedRecipe(id))
-  }
+const ErrorPopUp: React.FC = () => {
+  const error = useSelector((state: RootState) => state.likedRecipes.error);
+  const successMsg = useSelector(
+    (state: RootState) => state.likedRecipes.successMsg
+  );
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
   
-return(
+    if (error) {
+      dispatch(displayError(error));
+  
+      timeout = setTimeout(() => {
+        dispatch(displayError(null));
+        dispatch(displaySuccessMsg(null));
+      }, 2000);
+    }
+  
+    if (successMsg) {
+      dispatch(displaySuccessMsg(successMsg));
+  
+      timeout = setTimeout(() => {
+        dispatch(displayError(null));
+        dispatch(displaySuccessMsg(null));
+      }, 2000);
+    }
+  
+    return () => clearTimeout(timeout);
+  
+  }, [error, successMsg, dispatch]);
+
+  return (
     <>
-          <div
-        className={`${
-          error ? "fixed" : "hidden"
-        } flex justify-center items-center top-20 lg:top-24 z-20 w-full h-20`}
+      {error && (
+      <div
+        className={`fixed flex justify-center items-center top-20 lg:top-24 z-20 w-full h-20`}
       >
-        <div className="p-4 bg-gray-100 rounded-lg w-[250px]  text-xs xl:text-lg  lg:w-[400px]">
+        <div className="p-4 bg-gray-100 rounded-lg w-[250px] text-xs xl:text-lg lg:w-[400px]">
           <h1 className="text-xs font-semibold lg:text-sm">{error}</h1>
-          <h3 className="text-xxs lg:text-xs">Do you want to remove it?</h3>
-          <div className="w-full text-end text-xxs mt-3 lg:text-sm">
-            <button className="px-4 py-1 bg-customPink shadow-lg rounded-sm" onClick={() => handleDelete()}>Yes</button>
-            <button className="px-4 py-1 border-1 border-gray-100  shadow-lg  ml-2 rounded-sm" onClick={closePopUp}>No</button>
-          </div>
         </div>
       </div>
-    </>
-)
+      )}
 
-}
+      {successMsg && !error && (
+        <div
+          className={`
+           fixed
+           flex justify-center items-center top-20 z-20 w-full h-20`}
+        >
+          <div className="flex items-center justify-center bg-gray-100 rounded-lg w-[250px] p-4 text-xs xl:text-lg xl:w-[400px]">
+            <img src={addedRecipe} className="h-10" alt="" />
+            <h1 className="ml-5 text-center ">{successMsg}</h1>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ErrorPopUp;
