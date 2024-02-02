55ac144e27b369e3457e395af221a04bd24bab58
kngval <bkvalteocs@gmail.com>
2024-02-02 13:31:10 +0800

    fixed cors origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*", 
+    origin: "http://localhost:5173", 
+    methods: "GET,POST,PUT,PATCH,DELETE",
     credentials: true,
   })
 );
