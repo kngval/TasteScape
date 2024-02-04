4d0e8c8769d2013d88889f92cb69f4909a73f964
Kngval <bkvalteocs@gmail.com>
2024-02-04 13:27:45 +0800

    added new obj key

--- client/src/mock/foodMockData.ts
+++ client/src/mock/foodMockData.ts
@@ -54,56 +54,58 @@ export const FoodMock = {
     },
   ],
   analyzedInstructions: {
-    steps: [
-      {
-        number : 1,
-        step: "Take a large bowl mix in the ginger and garlic paste, yogurt, red chilly powder, turmeric powder, and salt.",
-      },
-      {
-        number : 2,
-        step: "Mix well to form a smooth and thick paste, add the chicken pieces to the masala paste and marinade for 4 hours.",
-      },
-      {
-        number : 3,
-        step: "Heat enough oil in a pan to deep fry the marinated chicken pieces. Deep fry the chicken pieces in batches till crisp and golden color. Note: The taste of the Chicken 65 depends mainly on the amount of time it gets marinated in the masala; it is best to marinate the chicken pieces the day before.",
-      },
-      {
-        number : 4,
-        step: "Take a large bowl mix in the ginger and garlic paste, yogurt, red chilly powder, turmeric powder, and salt.",
-      },
-      {
-        number : 5,
-        step: "Mix well to form a smooth and thick paste, add the chicken pieces to the masala paste and marinade for 4 hours.",
-      },
-      {
-        number : 6,
-        step: "Heat enough oil in a pan to deep fry the marinated chicken pieces. Deep fry the chicken pieces in batches till crisp and golden color. Note: The taste of the Chicken 65 depends mainly on the amount of time it gets marinated in the masala; it is best to marinate the chicken pieces the day before.",
-      },
-      {
-        number : 7,
-        step: "Take a large bowl mix in the ginger and garlic paste, yogurt, red chilly powder, turmeric powder, and salt.",
-      },
-      {
-        number : 8,
-        step: "Mix well to form a smooth and thick paste, add the chicken pieces to the masala paste and marinade for 4 hours.",
-      },
-      {
-        number : 9,
-        step: "Heat enough oil in a pan to deep fry the marinated chicken pieces. Deep fry the chicken pieces in batches till crisp and golden color. Note: The taste of the Chicken 65 depends mainly on the amount of time it gets marinated in the masala; it is best to marinate the chicken pieces the day before.",
-      },
-      {
-        number : 10,
-        step: "Take a large bowl mix in the ginger and garlic paste, yogurt, red chilly powder, turmeric powder, and salt.",
-      },
-      {
-        number : 11,
-        step: "Mix well to form a smooth and thick paste, add the chicken pieces to the masala paste and marinade for 4 hours.",
-      },
-      {
-        number : 12,
-        step: "Heat enough oil in a pan to deep fry the marinated chicken pieces. Deep fry the chicken pieces in batches till crisp and golden color. Note: The taste of the Chicken 65 depends mainly on the amount of time it gets marinated in the masala; it is best to marinate the chicken pieces the day before.",
-      },
-    ],
+    0:{
+      steps: [
+        {
+          number : 1,
+          step: "Take a large bowl mix in the ginger and garlic paste, yogurt, red chilly powder, turmeric powder, and salt.",
+        },
+        {
+          number : 2,
+          step: "Mix well to form a smooth and thick paste, add the chicken pieces to the masala paste and marinade for 4 hours.",
+        },
+        {
+          number : 3,
+          step: "Heat enough oil in a pan to deep fry the marinated chicken pieces. Deep fry the chicken pieces in batches till crisp and golden color. Note: The taste of the Chicken 65 depends mainly on the amount of time it gets marinated in the masala; it is best to marinate the chicken pieces the day before.",
+        },
+        {
+          number : 4,
+          step: "Take a large bowl mix in the ginger and garlic paste, yogurt, red chilly powder, turmeric powder, and salt.",
+        },
+        {
+          number : 5,
+          step: "Mix well to form a smooth and thick paste, add the chicken pieces to the masala paste and marinade for 4 hours.",
+        },
+        {
+          number : 6,
+          step: "Heat enough oil in a pan to deep fry the marinated chicken pieces. Deep fry the chicken pieces in batches till crisp and golden color. Note: The taste of the Chicken 65 depends mainly on the amount of time it gets marinated in the masala; it is best to marinate the chicken pieces the day before.",
+        },
+        {
+          number : 7,
+          step: "Take a large bowl mix in the ginger and garlic paste, yogurt, red chilly powder, turmeric powder, and salt.",
+        },
+        {
+          number : 8,
+          step: "Mix well to form a smooth and thick paste, add the chicken pieces to the masala paste and marinade for 4 hours.",
+        },
+        {
+          number : 9,
+          step: "Heat enough oil in a pan to deep fry the marinated chicken pieces. Deep fry the chicken pieces in batches till crisp and golden color. Note: The taste of the Chicken 65 depends mainly on the amount of time it gets marinated in the masala; it is best to marinate the chicken pieces the day before.",
+        },
+        {
+          number : 10,
+          step: "Take a large bowl mix in the ginger and garlic paste, yogurt, red chilly powder, turmeric powder, and salt.",
+        },
+        {
+          number : 11,
+          step: "Mix well to form a smooth and thick paste, add the chicken pieces to the masala paste and marinade for 4 hours.",
+        },
+        {
+          number : 12,
+          step: "Heat enough oil in a pan to deep fry the marinated chicken pieces. Deep fry the chicken pieces in batches till crisp and golden color. Note: The taste of the Chicken 65 depends mainly on the amount of time it gets marinated in the masala; it is best to marinate the chicken pieces the day before.",
+        },
+      ],
+    }
   },
   summary: "Chicken Adobo & Coconut Ginger Rice is a <b>gluten free and dairy free</b> recipe with 4 servings. One serving contains <b>730 calories</b>, <b>33g of protein</b>, and <b>34g of fat</b>. For <b>$1.92 per serving</b>, this recipe <b>covers 26%</b> of your daily requirements of vitamins and minerals. Head to the store and pick up rice, ginger, brown sugar, and a few other things to make it today. This recipe from Foodista has 3 fans. It works well as a main course. From preparation to the plate, this recipe takes about <b>1 hour and 30 minutes</b>. Taking all factors into account, this recipe <b>earns a spoonacular score of 58%</b>, which is solid. If you like this recipe, take a look at these similar recipes: <a href=\"https://spoonacular.com/recipes/chicken-adobo-coconut-ginger-rice-1369193\">Chicken Adobo & Coconut Ginger Rice</a>, <a href=\"https://spoonacular.com/recipes/thai-chicken-satay-and-peanut-sauce-ginger-coconut-rice-576937\">Thai Chicken Satay and Peanut Sauce + Ginger Coconut Rice</a>, and <a href=\"https://spoonacular.com/recipes/mango-chicken-brown-rice-salad-with-coconut-ginger-dressing-549074\">Mango, Chicken & Brown Rice Salad with Coconut-Ginger Dressing</a>.",
 };
