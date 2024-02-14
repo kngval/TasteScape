5215eab2cc7cffe084a382c33e63aa49d100014f
Kngval <bkvalteocs@gmail.com>
2024-02-14 16:13:04 +0800

    dup

--- client/src/mock/searchMock.ts
+++ client/src/mock/searchMock.ts
@@ -25,5 +25,32 @@ export const searchMock = [
         image: 'https://www.simplyrecipes.com/thmb/G0WpDNn_iGeaSImHqH2TGiG8rjY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Spaghetti-Carbonara-LEAD-7-82d6bacf7f3848a4943b14712ab205ff.jpg',
         isLiked: false
 
+    },
+    {
+        id:5,
+        title: 'Filipino Chicken Adobo',
+        image: 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FEdit%2F2022-10-Chicken-Adobo%2FChicken_Adobo',
+        isLiked: false
+    },
+    {
+        id:6,
+        title: 'Cordon Bleu',
+        image: 'https://www.simplyrecipes.com/thmb/G0WpDNn_iGeaSImHqH2TGiG8rjY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Spaghetti-Carbonara-LEAD-7-82d6bacf7f3848a4943b14712ab205ff.jpg',
+        isLiked: false
+
+    },
+    {
+        id:7,
+        title: 'Filipino Chicken Adobo',
+        image: 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_4:3/k%2FEdit%2F2022-10-Chicken-Adobo%2FChicken_Adobo',
+        isLiked: false
+
+    },
+    {
+        id:8,
+        title: 'Cordon Bleu',
+        image: 'https://www.simplyrecipes.com/thmb/G0WpDNn_iGeaSImHqH2TGiG8rjY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Simply-Recipes-Spaghetti-Carbonara-LEAD-7-82d6bacf7f3848a4943b14712ab205ff.jpg',
+        isLiked: false
+
     }
 ]
