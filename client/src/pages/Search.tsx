b6016feebc7196ac93f0d10664b183137d36adc4
kngval <bkvalteocs@gmail.com>
2024-02-28 15:59:00 +0800

    search page

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,11 +1,43 @@
+import { useSelector } from "react-redux";
 import { BottomNavbar } from "../components/BottomNavbar";
 import Navbar from "../components/Navbar";
+import { Recipes } from "../components/Recipes";
+import SearchForm from "../components/SearchForm";
+import { RootState } from "../Redux/store";
+import LoadingSpinner from "../components/LoadingSpinner";
+import ErrorPopUp from "../components/ErrorPopUp";
 
 const Search = () => {
+  const { searchedRecipes, loading } = useSelector(
+    (state: RootState) => state.recipes
+  );
   return (
     <>
       <Navbar />
-      <div></div>
+
+      <ErrorPopUp />
+      {/* SEARCH FORM */}
+      <div className="w-full h-[100px] relative">
+        <SearchForm />
+      </div>
+
+      {/* LOADING SPINNER */}
+      {loading && !searchedRecipes && <LoadingSpinner />}
+
+      {/* SEARCHED RECIPES */}
+      <div className="grid grid-cols-2 gap-2 p-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 my-[6rem]">
+        {searchedRecipes &&
+          searchedRecipes.map((recipe) => (
+            <div className="text-xs md:text-sm xl:text-md  bg-white   border-gray-200 border-2">
+              <Recipes
+                id={recipe.id}
+                title={recipe.title}
+                isLiked={recipe.isLiked}
+                image={recipe.image}
+              />
+            </div>
+          ))}
+      </div>
       <BottomNavbar />
     </>
   );
