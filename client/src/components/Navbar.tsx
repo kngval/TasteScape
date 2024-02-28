024e09cc7d0b1e9155cc2f4ae4daafd83150816f
kngval <bkvalteocs@gmail.com>
2024-02-28 15:55:14 +0800

    new design

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,30 +1,36 @@
 import { NavLink } from "react-router-dom";
+import NavBtns from "./NavBtns";
+import searchIcon from "../assets/svgs/search.svg";
+
 // import userSvg from "../assets/svgs/userIcon.svg";
 const Navbar: React.FC = () => {
   return (
     <>
-      <nav className=" text-sm lg:text-md sticky top-0 flex justify-center w-full bg-white p-4 shadow-md z-50">
-        <div className="w-[95%] justify-between flex items-center mx-5">
-          <div className="flex items-center">
-            <button></button>
-            <NavLink to="/" className="navbar-title text-xl font-semibold ">
+      <nav className=" text-sm lg:text-md sticky top-0 flex justify-center w-full bg-white px-4 py-2 z-[100]">
+        <div className="w-[95%] flex justify-between items-center">
+          <div className="flex items-center h-full">
+            <NavLink to="/home" className="navbar-title text-xl font-semibold ">
               TASTESCAPE
             </NavLink>
           </div>
-          <div className="hidden lg:flex list-none gap-10">
-            <li>
-              <NavLink to="/home">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/">Search</NavLink>
-            </li>
-            <li>
-              <NavLink to="/favorites">Favorites</NavLink>
-            </li>
+
+          <div className="hidden lg:flex ml-[8%] gap-2">
+            <NavBtns />
           </div>
-          <li className="list-none p-2 bg-[#FF6F6F] text-white hidden  lg:block">
-            <NavLink to="/my-recipes">Make Your Own</NavLink>
-          </li>
+
+          <div className="hidden lg:flex w-[20%] h-[70%] items-center">
+            <input
+              type="text"
+              className="w-full h-full bg-white rounded-s-md border-s-2 border-y-2 border-black outline-none p-2"
+              placeholder="Search for Recipes..."
+            />
+            <button
+              type="submit"
+              className="w-[50px] h-[40px] p-2 flex items-center justify-center  rounded-e-md border-y-2 border-e-2 border-black bg-white"
+            >
+              <img src={searchIcon} className="w-full h-full" alt="" />
+            </button>
+          </div>
         </div>
       </nav>
     </>
