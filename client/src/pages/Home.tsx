39eddb4d240ba21d854f9f955695d324171feb1f
kngval <bkvalteocs@gmail.com>
2024-04-24 22:57:31 +0800

    fixed api fetch

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,30 +1,48 @@
 //REACT & REDUX
 import { AppDispatch, RootState } from "../Redux/store";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 //Components
 import { Header } from "../components/Header";
 import { Recipes } from "../components/Recipes";
 import { BottomNavbar } from "../components/BottomNavbar";
 import Navbar from "../components/Navbar";
 import ErrorPopUp from "../components/ErrorPopUp";
+import SearchForm from "../components/SearchForm";
 //assets
 
 //Exported Functions
 import { fetchRecipes } from "../Redux/foodSlice";
-import SearchForm from "../components/SearchForm";
 import LoadingSpinner from "../components/LoadingSpinner";
+import axios from "axios";
+import Recipe from "../interfaces/IRecipes";
 const Home: React.FC = () => {
   //REDUX STATES
   const recipes = useSelector((state: RootState) => state?.recipes.recipes);
   const loading = useSelector((state: RootState) => state?.recipes.loading);
   const dispatch = useDispatch<AppDispatch>();
-  
+
+  const [firstMountRecipes, setFirstMountRecipes] = useState<Recipe[]>([])
 
   useEffect(() => {
-    dispatch(fetchRecipes("Korean"));
+    dispatch(fetchRecipes('American'));
+    fetchRecipes2();
   }, [dispatch]);
   
+  const fetchRecipes2 = async() => {
+    try {
+
+      const response = await axios.get(`http://localhost:3000/home/asian`, {
+      withCredentials: true,
+    });
+      
+      const asianRecipes = response.data.results;
+      setFirstMountRecipes(asianRecipes);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <Navbar />
@@ -40,7 +58,7 @@ const Home: React.FC = () => {
         <div className="recipe-wrapper mb-40 mt-32 lg:mt-32 px-4 lg:px-12 ">
       <div className="">
         <h1 className="trending  lg:ml-0 ml- text-xl lg:text-2xl mt-10 font-bold">
-          Trending Recipes
+          American  Recipes
         </h1>
 
           {/* Loader Spinner */}
@@ -51,7 +69,7 @@ const Home: React.FC = () => {
 
             {/* RECIPE  */}
           <div className="flex gap-2 ">
-            {recipes &&
+            {Array.isArray(recipes) && recipes.length > 0 &&
               recipes.map((recipe) => (
                 <div className="text-xs md:text-sm xl:text-md  bg-white w-[200px] sm:w-[300px] lg:w-[300px] border-gray-200 border-2">
                   <Recipes
@@ -61,7 +79,7 @@ const Home: React.FC = () => {
                     title={recipe.title}
                     isLiked={recipe.isLiked}
                   />
-                </div>
+                  </div>
               ))}
           </div>
         </div>
@@ -77,8 +95,8 @@ const Home: React.FC = () => {
               <LoadingSpinner />
             )}
           <div className="flex gap-2">
-            {recipes &&
-              recipes.map((recipe) => (
+            {Array.isArray(firstMountRecipes) &&
+              firstMountRecipes.map((recipe) => (
                 <div className="text-xs md:text-sm xl:text-md  bg-white w-[200px] sm:w-[300px] lg:w-[300px] border-gray-200 border-2">
                   <Recipes
                     key={recipe.id}
