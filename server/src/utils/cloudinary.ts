26957d9cbd38b01cdc15077a97fe74f791dcff26
kngval <bkvalteocs@gmail.com>
2024-04-24 23:07:00 +0800

    cloudinary config

---
+++ server/src/utils/cloudinary.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+export default cloudinary;
