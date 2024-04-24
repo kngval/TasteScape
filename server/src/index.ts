8fc1cecb8c6f82502fcca8011aec864b0b07f466
kngval <bkvalteocs@gmail.com>
2024-04-24 23:05:35 +0800

    increased payload size

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,36 +1,33 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 import foodSearch from "./routes/foodSearch";
 import likedRoutes from "./routes/likedRoutes";
 import createdFoodRoutes from "./routes/createdFoodRoute";
 import "dotenv/config";
+import { connectToDb } from "./db/db";
 const app = express();
+const PORT = process.env.PORT;
 
-//middleware
-app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: "GET,POST,PUT,PATCH,DELETE",
-    credentials: true,
+connectToDb()
+  .then(() => {
+    //middleware
+    app.use(
+      cors({
+        origin: "http://localhost:5173",
+        methods: "GET,POST,PUT,PATCH,DELETE",
+        credentials: true,
+      })
+    );
+    app.use(express.json({ limit: "50mb" }));
+    app.options("/favorites/:id", cors()); // Handle OPTIONS for DELETE
+    app.use("/Home", foodSearch); //Route for first mount in /Home
+    app.use("/favorites", likedRoutes); //Route for liked recipes
+    app.use("/my-recipes", createdFoodRoutes); //Route for creating Recipes
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
   })
-);
-app.options("/favorites/:id", cors()); // Handle OPTIONS for DELETE
-app.use("/Home", foodSearch); //Route for first mount in /Home
-app.use("/favorites", likedRoutes); //Route for liked recipes
-app.use("/my-recipes",createdFoodRoutes); //Route for creating Recipes
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(
-        `Server listening on port http://localhost:${process.env.PORT}...`
-      )
-    )
-  )
-  .catch((error: Error) => console.log(error));
-
-mongoose.connection.on("error", (error: Error) => {
-  console.log("MongoDB Connection Error : ", error);
-});
+  .catch((error) => {
+    console.log("Failed to connect to MongoDB: ", error);
+  });
