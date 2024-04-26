5d97bc6c71b0d42c6c2e56f960eeaa733f178f2f
kngval <bkvalteocs@gmail.com>
2024-04-26 22:29:28 +0800

    added conditional rendering

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -6,11 +6,14 @@ import SearchForm from "../components/SearchForm";
 import { RootState } from "../Redux/store";
 import LoadingSpinner from "../components/LoadingSpinner";
 import ErrorPopUp from "../components/ErrorPopUp";
+// import { useState } from "react";
 
 const Search = () => {
   const { searchedRecipes, loading } = useSelector(
     (state: RootState) => state.recipes
   );
+  // const [queryStr, setQueryStr] = useState('');
+
   return (
     <>
       <Navbar />
@@ -25,8 +28,13 @@ const Search = () => {
       {loading && !searchedRecipes && <LoadingSpinner />}
 
       {/* SEARCHED RECIPES */}
+      {searchedRecipes && searchedRecipes.length > 0 && (
+        <div>
+        <div>
+          <h1>Search results for</h1>
+        </div>
       <div className="grid grid-cols-2 gap-2 p-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 my-[6rem]">
-        {searchedRecipes &&
+        {searchedRecipes && searchedRecipes.length > 0 &&
           searchedRecipes.map((recipe) => (
             <div className="text-xs md:text-sm xl:text-md  bg-white   border-gray-200 border-2">
               <Recipes
@@ -38,6 +46,8 @@ const Search = () => {
             </div>
           ))}
       </div>
+      </div>
+      )}
       <BottomNavbar />
     </>
   );
