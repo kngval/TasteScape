c47010970d2c480254aeea8e94fae84acb6fa5cf
kngval <bkvalteocs@gmail.com>
2024-05-01 23:25:17 +0800

    id parameter for route

--- server/src/routes/createdFoodRoute.ts
+++ server/src/routes/createdFoodRoute.ts
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import { createRecipe, fetchCreatedRecipes } from "../controllers/CreatingRecipes";
+import { createRecipe, fetchCreatedRecipes, getCreatedRecipeDetails } from "../controllers/CreatingRecipes";
 
 router.get('/',fetchCreatedRecipes)
 router.post('/createRecipe', createRecipe);
-
+router.get('/:id',getCreatedRecipeDetails);
 
 export default router;
