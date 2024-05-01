ef28071a7d017820a1a827202c7757f7ba1d70b3
kngval <bkvalteocs@gmail.com>
2024-05-01 23:26:09 +0800

    assets

---
+++ client/src/auth/Login.tsx
@@ -0,0 +1,14 @@
+const Login = () => {
+  return (
+    <div className="grid grid-cols-2">
+      <div>
+        asdsa
+        <img src="" alt="" />
+      </div>
+
+      <div>Form</div>
+    </div>
+  );
+};
+
+export default Login;
