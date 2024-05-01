52831608c51e60b2d1ecde84d5c491430a4e297d
kngval <bkvalteocs@gmail.com>
2024-05-01 23:20:44 +0800

    route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Profile from "./pages/Profile";
 import RecipeDetails from "./pages/RecipeDetails";
 import Search from "./pages/Search";
 import CreateRecipe from "./pages/CreateRecipe";
+import Login from "./auth/Login";
 
 
 function App() {
@@ -13,10 +14,10 @@ function App() {
     <BrowserRouter>
       <div>
         <Routes>
+          <Route path="/" element={<Login />}/>
           <Route path="/home" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/search" element={<Search />} />
-
           <Route path="/my-recipes" element={<MyRecipes />} />
           <Route path="/my-recipes/createRecipe" element={<CreateRecipe />}/>
           <Route path="/favorites" element={<LikedRecipes />} />
