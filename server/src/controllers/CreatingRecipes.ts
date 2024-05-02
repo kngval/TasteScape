f288c27ace3b0ad7572e4c0d8bdbdb0d2b2bb994
kngval <bkvalteocs@gmail.com>
2024-05-02 21:58:05 +0800

    controller for deleting

--- server/src/controllers/CreatingRecipes.ts
+++ server/src/controllers/CreatingRecipes.ts
@@ -2,7 +2,6 @@ import { getDb } from "../db/db";
 import { Request, Response } from "express";
 import { Collection, Document, ObjectId } from "mongodb";
 import cloudinary from "../utils/cloudinary";
-import RecipeModel from "../models/CreateRecipe";
 interface Recipe {
   title: string;
   description: string;
@@ -91,5 +90,52 @@ export const getCreatedRecipeDetails = async (req: Request, res: Response) => {
       : res.status(500).json({ error: "Nothing found in db..." });
   } catch (error) {
     console.log(error);
+  }
+};
+
+//DELETE CREATED RECIPE
+export const deleteCreatedRecipe = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const db = getDb();
+    const response = await db
+      .collection("likedrecipes") // Target the "likedrecipes" collection
+      .deleteOne({ _id: new ObjectId(id) }); // Delete the recipe by its ID
+    console.log(response);
+    if (response.deletedCount === 1) {
+      res.status(200).json({ message: "Recipe deleted successfully" });
+    } else {
+      res.status(404).json({ error: "Recipe not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Failed to delete recipe" });
+  }
+};
+
+export const addLikedCreatedRecipe = async (req: Request, res: Response) => {
+  const { _id, title, image }: { _id: string; title: string; image: string } =
+    req.body;
+  console.log("Received Data:", { _id, title, image });
+
+  try {
+    const db = getDb();
+    const exists = await db
+      .collection("likedrecipes")
+      .findOne({ _id: new ObjectId(_id) });
+    if (!exists) {
+      const LikedRecipes = await db.collection("likedrecipes").insertOne({
+        _id: new ObjectId(_id),
+        title,
+        image,
+        isLiked: true,
+      });
+      res.status(200).json(LikedRecipes);
+    } else {
+      throw Error("Already Liked this Recipe");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
   }
 };
