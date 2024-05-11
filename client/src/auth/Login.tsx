5f46bcba3fc33f92b909e9740223d2aa74836da1
kngval <bkvalteocs@gmail.com>
2024-05-11 20:09:57 +0800

    login form

--- client/src/auth/Login.tsx
+++ client/src/auth/Login.tsx
@@ -1,12 +1,116 @@
+import TasteScape from "../assets/svgs/TasteScape.svg";
+import lock from "../assets/svgs/lock.svg";
+import emailSvg from "../assets/svgs/email.svg";
+import recipebook from "./recipebook.svg";
+import chefslide from "./chefslide.svg";
+import grocery from "./grocery.svg"
+import { useEffect, useState } from "react";
 const Login = () => {
-  return (
-    <div className="grid grid-cols-2">
-      <div>
-        asdsa
-        <img src="" alt="" />
-      </div>
+  const sliderImgs = [chefslide, recipebook,grocery];
 
-      <div>Form</div>
+  const [currIndex, setCurrIndex] = useState<number>(0);
+  const [direction, setDirection] = useState<number>(1);
+
+  useEffect(() => {
+    const autoSlider = setInterval(() => {
+      setCurrIndex((prevIndex) => {
+        if (prevIndex === sliderImgs.length - 1 && direction === 1) {
+          setDirection(-1);
+          return prevIndex - 1;
+        } else if (prevIndex === 0 && direction - 1) {
+          setDirection(1);
+          return prevIndex + 1;
+        } else {
+          return prevIndex + direction;
+        }
+      });
+    }, 5000);
+
+    return () => clearInterval(autoSlider);
+  }, [currIndex]);
+  return (
+    <div className="lg:flex items-center">
+      <div className="hidden relative lg:flex items-center justify-center w-[600px] xl:w-[800px] 2xl:w-[1000px] h-screen bg-customPink">
+        <div className="slide-container">
+          <div className="w-[550px]  overflow-hidden">
+            <div
+              className="grid grid-cols-3 items-center gap-[5rem] "
+              style={{
+                width: `${sliderImgs.length * 100}%`,
+                transform: `translateX(-${currIndex * 33}%)`,
+                transition: `0.5s ease-in-out`,
+              }}
+            >
+              {sliderImgs.map((img) => (
+                <img src={img} alt="" className="w-[700px]" />
+              ))}
+            </div>
+          </div>
+          <div className="text-center text-[#ffffff]">
+            <h1 className=" text-[20px] font-bold mt-10 mb-2">
+              {currIndex === 0 ? "Turn your Ideas into reality " : currIndex === 1 ? "Explore a wide variety of Recipes" : "Craft your own Recipes"}
+            </h1>
+            <p className="text-sm px-4">
+              {currIndex === 0 ? "Create and Innovate your own culinary legacy" : currIndex === 1 ? "Discover vast recipes from around the world" : "Dive into the world of cooking and unleash your creativity by crafting your own delicious recipes"}
+            </p>
+          </div>
+        </div>
+
+            <div className="absolute bottom-10 flex gap-2">
+                <div className={`w-[8px] h-[8px] rounded-full ${currIndex === 0 ? "bg-white" : "bg-[#ffffff46]"}`}></div>
+                <div className={`w-[8px] h-[8px] rounded-full ${currIndex === 1 ? "bg-white" : "bg-[#ffffff46]"}`}></div>
+                <div className={`w-[8px] h-[8px] rounded-full ${currIndex === 2 ? "bg-white" : "bg-[#ffffff46]"}`}></div>
+            </div>
+
+      </div>
+      <form
+        action=""
+        className="login-form w-[300px] mt-[15rem] m-auto lg:m-auto font-sans"
+      >
+        {/* TITLE */}
+        <div className="flex justify-center mb-5">
+          <img src={TasteScape} alt="" />
+        </div>
+
+        <div className="grid gap-2 text-xs">
+          {/* EMAIL INPUT */}
+          <div className="flex border-[1px]  border-[#e7e7e7] py-2 px-4 gap-2 bg-white rounded-md">
+            <img src={emailSvg} alt="" className="w-[25px] " />
+
+            <input
+              type="text"
+              className="w-full outline-none"
+              placeholder="Email"
+              required
+            />
+          </div>
+
+          {/* PASSWORD INPUT */}
+          <div className="flex border-[1px]  border-[#e7e7e7] py-2 px-4 gap-2 bg-white rounded-md">
+            <img src={lock} alt="" className="w-[21px] " />
+
+            <input
+              type="password"
+              className="w-full outline-none"
+              placeholder="Password"
+              required
+            />
+          </div>
+          {/* SIGN-IN BTN */}
+          <div className="signin-btn flex justify-center my-5 text-xs">
+            <button className="bg-customPink text-white w-full py-3 rounded-md">
+              Sign In
+            </button>
+          </div>
+
+          <div className="text-center">
+            <h3 className="text-[#a1a1a1]">
+              Dont have an account?{" "}
+              <span className="text-customPink">Create One</span>
+            </h3>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
