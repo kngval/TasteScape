355532a6cb2478326f42a5b16241fc4100b6e237
kngval <bkvalteocs@gmail.com>
2024-05-13 23:13:14 +0800

    auth slice

--- client/src/Redux/store.ts
+++ client/src/Redux/store.ts
@@ -1,20 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
-import recipeReducer from './foodSlice'
-import chosenRecipeReducer from './chosenRecipeSlice'
-import likedRecipeReducer from './likedRecipeSlice';
-
+import recipeReducer from "./foodSlice";
+import chosenRecipeReducer from "./chosenRecipeSlice";
+import likedRecipeReducer from "./likedRecipeSlice";
+import authReducer from "./authSlice";
 
 export const store = configureStore({
-    reducer:{
-        recipes: recipeReducer,
-        chosenRecipe: chosenRecipeReducer,
-        likedRecipes: likedRecipeReducer
-    },
-    middleware: (getDefaultMiddleware) =>
+  reducer: {
+    recipes: recipeReducer,
+    chosenRecipe: chosenRecipeReducer,
+    likedRecipes: likedRecipeReducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false, // Disable the serializable state check
     }),
-})
+});
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch =  typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch;
