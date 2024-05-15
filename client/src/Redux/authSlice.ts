fc8764bd4afd9f687b9fb12fa9b9433c32c30bbc
kngval <bkvalteocs@gmail.com>
2024-05-15 23:05:53 +0800

    loading + error handling

--- client/src/Redux/authSlice.ts
+++ client/src/Redux/authSlice.ts
@@ -7,14 +7,22 @@ type UserInfo = {
   token: string;
 };
 
+
 type AuthState = {
   userInfo: UserInfo | null;
+  error: string | null | undefined;
+  loading:boolean;
 };
 
+const userInfoFromLocalStorage = localStorage.getItem("userInfo") ? localStorage.getItem("userInfo") : null;
+const parsedUserInfo = userInfoFromLocalStorage
+  ? JSON.parse(userInfoFromLocalStorage)
+  : null;
+
 const initialState: AuthState = {
-  userInfo: localStorage.getItem("userInfo")
-    ? JSON.parse(localStorage.getItem("userInfo")!)
-    : null,
+  userInfo: parsedUserInfo,
+  error: null,
+  loading:false,
 };
 
 export const loginUser = createAsyncThunk(
@@ -27,13 +35,14 @@ export const loginUser = createAsyncThunk(
           password,
         });
 
-        if (res) {
-          console.log(res.data);
+        if(res || res !== null || undefined){
+          console.log(res.data)
           return res.data;
         }
       }
-    } catch (error) {
-      console.log(error);
+    } catch (error:any) {
+      console.log(error.response.data.error)
+      throw new Error(error.response.data.error)
     }
   }
 );
@@ -46,11 +55,23 @@ const authSlice = createSlice({
       state.userInfo = null;
       localStorage.removeItem("userInfo");
     },
+  
   },
   extraReducers: (builder) => {
+    builder.addCase(loginUser.pending, (state) => {
+      state.loading = true;
+    });
     builder.addCase(loginUser.fulfilled, (state, action) => {
       state.userInfo = action.payload;
-      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+      state.userInfo ? localStorage.setItem("userInfo", JSON.stringify(action.payload)) : null;
+      state.loading = false;
+    });
+    builder.addCase(loginUser.rejected, (state, action) => {
+      state.error = action.error.message;
+      state.loading = false;
+      console.log(state.error)
+      state.userInfo = null;
+      localStorage.removeItem("userInfo")
     });
   },
 });
